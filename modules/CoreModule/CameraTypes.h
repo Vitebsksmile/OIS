1f17c68bfb8a0eb7b6e8f8c49980beb78e6e405f
Vitebsksmile <vitebsksmile@mail.ru>
2026-07-24 01:40:38 +0300

    feature: add CameraTypes.h

--- modules/CoreModule/CameraTypes.h
+++ modules/CoreModule/CameraTypes.h
@@ -1,5 +1,82 @@
 #ifndef CAMERATYPES_H
 #define CAMERATYPES_H
 
+#include <cstdint>
+#include <string>
+
+
+namespace OIS::Core
+{
+
+using CameraId = std::uint64_t;
+
+enum class CameraTechnology
+{
+    GigE,
+    USB3Vision,
+    CameraLink,
+    CoaXPress
+};
+
+enum class CameraState
+{
+    Unknown,
+    Disconnected,
+    Connected,
+    Grabbing,
+    Error
+};
+
+enum class PixelFormat
+{
+    Unknown,
+
+    Mono8,
+    Mono10,
+    Mono12,
+    Mono16,
+
+    BayerRG8,
+    BayerRG12,
+
+    RGB8,
+    BGR8,
+
+    YUV422
+};
+
+enum class TriggerMode
+{
+    Continuous,
+    Software,
+    Hardware
+};
+
+struct CameraConfig
+{
+    CameraId id{0};
+
+    CameraTechnology technology{ CameraTechnology::GigE };
+
+    std::string serialNumber;
+
+    std::string ipAddress;
+
+    std::uint32_t width{ 0 };
+
+    std::uint32_t height{ 0 };
+
+    PixelFormat pixelFormat{ PixelFormat::Unknown };
+
+    double fps{ 0.0 };
+
+    TriggerMode triggerMode{ TriggerMode::Continuous };
+
+    double exposure{ 0.0 };
+
+    double gain{ 0.0 };
+};
+
+}
 
 #endif // CAMERATYPES_H
