0813db5f742a9473882c9d9b44d1f8d363e21bf1
Vitebsksmile <vitebsksmile@mail.ru>
2026-07-25 00:08:12 +0300

    feature: add ICameraFactory

---
+++ modules/CameraManagerModule/interface/ICameraFactory.h
@@ -0,0 +1,21 @@
+#ifndef ICAMERAFACTORY_H
+#define ICAMERAFACTORY_H
+
+#include <memory>
+
+#include "CameraTypes.h"
+
+
+class ICamera;
+
+class ICameraFactory
+{
+public:
+    virtual ~ICameraFactory() = default;
+
+    virtual std::unique_ptr<ICamera> createCamera(
+        const OIS::Core::CameraConfig& config
+        ) = 0;
+};
+
+#endif // ICAMERAFACTORY_H
