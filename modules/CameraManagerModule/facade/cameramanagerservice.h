3085ed1e4012df7803cd20cd61314496c7d11902
Vitebsksmile <vitebsksmile@mail.ru>
2026-07-27 00:28:16 +0300

    feature: change CameraManagerService

--- modules/CameraManagerModule/facade/cameramanagerservice.h
+++ modules/CameraManagerModule/facade/cameramanagerservice.h
@@ -2,16 +2,66 @@
 #define CAMERAMANAGERSERVICE_H
 
 #include <QObject>
+#include <memory>
+#include <unordered_map>
+
+#include "CameraTypes.h"
+#include "Frame.h"
 
 #include "ICameraManagerService.h"
+#include "IFrameObserver.h"
 
 
-class CameraManagerService : public ICameraManagerService
+class ICamera;
+class ICameraFactoryRegistry;
+
+class CameraManagerService
+    : public ICameraManagerService,
+      public IFrameObserver
 {
     Q_OBJECT
 
 public:
-    explicit CameraManagerService(QObject* parent = nullptr);
+    explicit CameraManagerService(std::unique_ptr<ICameraFactoryRegistry> factoryRegistry);
+
+    ~CameraManagerService() override;
+
+    bool initialize() override;
+
+    void shutdown() override;
+
+    OIS::Core::CameraId addCamera(const OIS::Core::CameraConfig& config) override;
+
+    bool removeCamera(OIS::Core::CameraId id) override;
+
+    bool startCamera(OIS::Core::CameraId id) override;
+
+    void stopCamera(OIS::Core::CameraId id) override;
+
+signals:
+
+    void frameReady(OIS::Core::Frame frame);
+
+    void cameraStateChanged(
+        OIS::Core::CameraId id,
+        OIS::Core::CameraState state
+    );
+
+    void errorOccured(
+        OIS::Core::CameraId id,
+        QString message
+    );
+
+private:
+
+    void onFrameReady(OIS::Core::Frame frame) override;
+
+    std::unique_ptr<ICameraFactoryRegistry> m_factoryRegistry;
+
+    std::unordered_map<
+        OIS::Core::CameraId,
+        std::unique_ptr<ICamera>
+        > m_cameras;
 
 };
 
